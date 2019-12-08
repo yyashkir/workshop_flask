09df4389424fcc35e11114c9d19a18f8ac6a29b9
Dmytro Yashkir <dima@dyashkir.com>
2019-12-08 18:49:58 +0900

    Flask web app now calling matrix app; provides arguments; result give a link to the resulting matrix file

--- flask_app/flaskr/app_runner.py
+++ flask_app/flaskr/app_runner.py
@@ -1,10 +1,13 @@
 import functools
 import subprocess
 import threading
+import os
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+
+from werkzeug.utils import secure_filename
 
 
 import uuid
@@ -12,30 +15,43 @@ tasks = {}
 
 bp = Blueprint('app_runner', __name__, url_prefix='/app_runner')
 
+tmp_folder = 'tmp'
+
 @bp.route('/start', methods=['GET', 'POST'])
 def start():
-    print('start')
     if request.method == 'POST':
-        x1 = request.form['power']
-        x2 = request.form['expansion']
-        x3 = request.form['matrix_file']
-
-        print(x1,x2,x3)
 
         task_id = uuid.uuid4().hex
         error = None
 
-        if not x1:
-            error = 'no x1'
-        elif not x2:
-            error = 'no x2'
+        power = request.form['power']
+        expansion = request.form['expansion']
+        matrix_file = request.files['matrix_file']
+
+        if not power:
+            error = 'no power'
+        elif not expansion:
+            error = 'no expansion'
+        elif not matrix_file:
+            error = 'no matrix file'
 
         if error is None:
-            proc = subprocess.Popen(f"python tester.py --x1={x1} --x2={x2}", shell=True, stdout=subprocess.PIPE)
+            filename = secure_filename(matrix_file.filename)
+            matrix_save_fname = os.path.join('tmp', filename)
+            matrix_file.save(matrix_save_fname)
+
+            output_fname = f'out_{filename}'
+            output_path= os.path.join('flaskr/static/files/', output_fname)
+            exec_line = f"../matrix_app/matrix_prg {power} {expansion} {matrix_save_fname} {output_path}"
+            print(exec_line)
+
+            proc = subprocess.Popen(exec_line, shell=True, stdout=subprocess.PIPE)
             res = ''
             for line in proc.stdout:
                 res = res + str(line)
-            tasks[task_id] = { 'result_data' : res }
+            tasks[task_id] = { 'result_data' : res, 
+                    'output_fname' : output_fname,
+                    'output_path' : output_path}
             return redirect(url_for('app_runner.result', task_id=task_id))
 
         flash(error)
@@ -50,6 +66,8 @@ def result(task_id):
         flash(error)
         result_data=None
     else:
-        result_data=task['result_data']
+        with open(task['output_path'], 'r') as myfile:
+            data=myfile.read()
+            result_data={ 'download_path' : url_for('static', filename=f"files/{task['output_fname']}"), 'content' : data }
 
     return render_template('app_runner/result.html', result_data=result_data)
